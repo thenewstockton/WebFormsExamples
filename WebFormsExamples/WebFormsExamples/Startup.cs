057fdb911acabb2c07d9ba378ac4a6d5e8135da3
phillipko <phillip56@hotmail.com>
2018-01-29 00:06:04 +0800

    First commit

---
+++ WebFormsExamples/WebFormsExamples/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebFormsExamples.Startup))]
+namespace WebFormsExamples
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
